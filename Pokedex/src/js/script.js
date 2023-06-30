2c18e8ede55928cb322c290be44c74475878e3b6
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2023-06-30 16:43:16 -0300

    modified:   Pokedex/src/js/script.js

--- Pokedex/src/js/script.js
+++ Pokedex/src/js/script.js
@@ -1,25 +1,34 @@
 const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
 const pokemonsCard = document.querySelectorAll('.pokemon-card')
+
  
+
 listaSelecaoPokemons.forEach(pokemon => {
-    
-    pokemon.addEventListener('click',() =>{
- 
-        const cartaoPokemonAberto = document.querySelector('.open')
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.open')        
         cartaoPokemonAberto.classList.remove('open')
+
  
+
         const idPokemonSelecionado = pokemon.attributes.id.value
+
  
-        const idCartaoPokemonParaAbrir = idPokemonSelecionado + '-card'
-        const cartaoPokemonParaAbrir = document.getElementById(idCartaoPokemonParaAbrir)
+
+        const idDoCartaoPokemonParaAbrir = idPokemonSelecionado  + '-card'
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
         cartaoPokemonParaAbrir.classList.add('open')
+
  
+
         const pokemonAtivoNaListagem = document.querySelector('.ativo')
         pokemonAtivoNaListagem.classList.remove('ativo')
+
  
-        const pokemonSelecionadoNaListagem = document.querySelector(idPokemonSelecionado)
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
         pokemonSelecionadoNaListagem.classList.add('ativo')
+
  
+
     })
- 
-});
+})
